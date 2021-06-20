e8e5c0060bdd38c13b0b540c52c1c98864e39b4f
yanna <yanna@DESKTOP-HE444UF>
2021-06-20 15:51:37 +0200

    Ajoutez des fichiers projet.

---
+++ Bot Discord Jeu de Carte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bot_Discord_Jeu_de_Carte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
